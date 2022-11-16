6dcbfd7e3b9ace75cfb925429825cd1b5c86711a
Abdullah Bashir <abdullahbr98@gmail.com>
2022-11-16 15:11:09 +0500

    UI changes

---
+++ main.js
@@ -0,0 +1,17 @@
+const recentFunction = (idDisplay,idNoDisplay,idNoDisplay2)=>{
+    idDisplay.style.display = "inline";
+    idNoDisplay.style.display = "none";
+    idNoDisplay2.style.display = "none";
+}
+
+const directoryFunction = (idDisplay,idNoDisplay,idNoDisplay2)=>{
+    idDisplay.style.display = "inline";
+    idNoDisplay.style.display = "none";
+    idNoDisplay2.style.display = "none";
+}
+
+const uploadFunction = (idDisplay,idNoDisplay,idNoDisplay2)=>{
+    idDisplay.style.display = "inline";
+    idNoDisplay.style.display = "none";
+    idNoDisplay2.style.display = "none";
+}
